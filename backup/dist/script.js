2ab0365142eb8bfcd45abd42d659253709435d18
nat-davydova <natti.davydova@gmail.com>
2021-09-10 11:25:34 +0300

    feat(slider): add basic slider function

--- backup/dist/script.js
+++ backup/dist/script.js
@@ -9,8 +9,24 @@ function getAndPrepareSlides(slidesOptions) {
 function setSlideIntoUI(slide) {
     slidesOptions.innerHTML = slide;
 }
+function getCurrentSlideIndex() {
+    const slideValue = slidesOptions.innerHTML;
+    const slides = getAndPrepareSlides(slidesOptions);
+    return slides.indexOf(slideValue);
+}
+function mapSlides(slides, ms) {
+    setTimeout(function slider() {
+        const currentSlideIndex = getCurrentSlideIndex();
+        const newSlideIndex = slides[currentSlideIndex + 1]
+            ? currentSlideIndex + 1
+            : 0;
+        setSlideIntoUI(slides[newSlideIndex]);
+        setTimeout(slider, ms);
+    }, ms);
+}
 function textSliderInit() {
     const slides = getAndPrepareSlides(slidesOptions);
     setSlideIntoUI(slides[0]);
+    mapSlides(slides, 1000);
 }
 textSliderInit();
--- backup/src/script.ts
+++ backup/src/script.ts
@@ -11,9 +11,30 @@ function setSlideIntoUI(slide) {
   slidesOptions.innerHTML = slide;
 }
 
+function getCurrentSlideIndex() {
+  const slideValue = slidesOptions.innerHTML;
+  const slides = getAndPrepareSlides(slidesOptions);
+  return slides.indexOf(slideValue);
+}
+
+function mapSlides(slides, ms) {
+  setTimeout(function slider() {
+    const currentSlideIndex = getCurrentSlideIndex();
+    const newSlideIndex = slides[currentSlideIndex + 1]
+      ? currentSlideIndex + 1
+      : 0;
+
+    setSlideIntoUI(slides[newSlideIndex]);
+
+    setTimeout(slider, ms);
+  }, ms);
+}
+
 function textSliderInit() {
   const slides = getAndPrepareSlides(slidesOptions);
   setSlideIntoUI(slides[0]);
+
+  mapSlides(slides, 1000);
 }
 
 textSliderInit();
