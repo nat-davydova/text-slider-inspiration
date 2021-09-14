5297a378de559eb4e23ab0eda0a8aa6812974f37
nat-davydova <natti.davydova@gmail.com>
2021-09-14 11:28:04 +0300

    chore(global): remove typescript cause codepen cannot handle it properly

--- backup/dist/script.js
+++ backup/dist/script.js
@@ -1,40 +1,50 @@
-"use strict";
 const slider = document.querySelector(".text-slider");
 const slidesOptions = slider.querySelector("[data-options]");
+
 function getAndPrepareSlides(slidesOptions) {
-    const slidesOptionsList = slidesOptions.dataset.options;
-    const slidesFromOptions = slidesOptionsList.split(", ");
-    return slidesFromOptions;
+  const slidesOptionsList = slidesOptions.dataset.options;
+  const slidesFromOptions = slidesOptionsList.split(", ");
+  return slidesFromOptions;
 }
+
 function setSlideIntoUI(slide) {
-    const newSlideNode = document.createElement("span");
-    newSlideNode.textContent = slide;
-    newSlideNode.style.opacity = 0;
-    newSlideNode.style.transition = "opacity .2s linear 0s";
-    slidesOptions.innerHTML = "";
-    slidesOptions.appendChild(newSlideNode);
-    setTimeout(() => {
-        newSlideNode.style.opacity = 1;
-    }, 0);
+  const newSlideNode = document.createElement("span");
+  newSlideNode.textContent = slide;
+
+  newSlideNode.style.opacity = 0;
+  newSlideNode.style.transition = "opacity .2s linear 0s";
+
+  slidesOptions.innerHTML = "";
+  slidesOptions.appendChild(newSlideNode);
+  setTimeout(() => {
+    newSlideNode.style.opacity = 1;
+  }, 0);
 }
+
 function getCurrentSlideIndex() {
-    const slideValue = slidesOptions.textContent;
-    const slides = getAndPrepareSlides(slidesOptions);
-    return slides.indexOf(slideValue);
+  const slideValue = slidesOptions.textContent;
+  const slides = getAndPrepareSlides(slidesOptions);
+  return slides.indexOf(slideValue);
 }
+
 function mapSlides(slides, ms) {
-    setTimeout(function slider() {
-        const currentSlideIndex = getCurrentSlideIndex();
-        const newSlideIndex = slides[currentSlideIndex + 1]
-            ? currentSlideIndex + 1
-            : 0;
-        setSlideIntoUI(slides[newSlideIndex]);
-        setTimeout(slider, ms);
-    }, ms);
+  setTimeout(function slider() {
+    const currentSlideIndex = getCurrentSlideIndex();
+    const newSlideIndex = slides[currentSlideIndex + 1] ?
+    currentSlideIndex + 1 :
+    0;
+
+    setSlideIntoUI(slides[newSlideIndex]);
+
+    setTimeout(slider, ms);
+  }, ms);
 }
+
 function textSliderInit() {
-    const slides = getAndPrepareSlides(slidesOptions);
-    setSlideIntoUI(slides[0]);
-    mapSlides(slides, 1500);
+  const slides = getAndPrepareSlides(slidesOptions);
+  setSlideIntoUI(slides[0]);
+
+  mapSlides(slides, 1500);
 }
+
 textSliderInit();
